6a04de068e26eefd393c6477e0f2af204435eabe
nicolocamassa <camassanicolo03@gmail.com>
2024-02-12 17:31:08 +0100

    [BONUS] header and footer included in characters

--- resources/views/charters.blade.php
+++ resources/views/charters.blade.php
@@ -1,12 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
 </head>
+
 <body>
-    <h1>Personaggi</h1>
+    @include('partials.header')
+
+    <main class="bg-light">
+        <h1>PERSONAGGI</h1>
+    </main>
+
+    @include('partials.footer')
 </body>
+
 </html>
