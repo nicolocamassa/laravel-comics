8182457db5febfad4cfd1ec673bbbfbc93ef6187
nicolocamassa <camassanicolo03@gmail.com>
2024-02-13 16:31:24 +0100

    [UPDATE] print comic infos

--- resources/views/comic-detail.blade.php
+++ resources/views/comic-detail.blade.php
@@ -1,5 +1,17 @@
 @extends('layout.app')
 
 @section('content')
-    <div>{{ $comic['series'] }}</div>
+    <div class="blue-banner">
+        <div class="smaller-content">
+            <img src="{{ $comic['thumb'] }}" alt="Comic cover">
+        </div>
+    </div>
+    <div class="smaller-content bg-white-content">
+        <h2>{{ $comic['title'] }}</h2>
+        <div class="green-banner">
+            <div class="price">{{ $comic['price'] }}</div>
+            <div class="aviability"></div>
+        </div>
+        <p>{{ $comic['description'] }}</p>
+    </div>
 @endsection
